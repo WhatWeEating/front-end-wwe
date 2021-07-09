4134d1009bc1fe04516a675f6e1f32ec737fec62
mcraig2342 <matthewcookecraig10@gmail.com>
2021-07-09 11:17:24 -0600

    Add return statement

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
@@ -14,6 +14,7 @@ class App extends Component {
   }
 
     render () {
+      return "What we eating"
   }
 
 }
