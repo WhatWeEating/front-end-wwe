a0be272ce66cc34ad80bd7aea7d9d6d3087e062b
robertjosephderouin <robertjosephderouin@gmail.com>
2021-07-13 10:30:36 -0700

    Add selection page testing

---
+++ cypress/integration/Selection.spec.js
@@ -0,0 +1,41 @@
+describe('Selection page and navigation to it', () => {
+    beforeEach(() => {
+      cy.stub()
+      cy.visit('http://localhost:3000')
+      cy.interceptRestaurantsFetch()
+    })
+
+	it('should be able to see all the landing page contents in preparation to navigate to the selection', () => {
+		cy.get('.LandingPage')
+			.find('h1')
+			.should('have.text', 'Find a perf place to go eat')
+			.get('.input-container > i')
+			.should('have.class', 'fas fa-search')
+			.should('be.visible')
+			.get('input[name="search"]')
+			.should('be.visible')
+			.get('.burger')
+			.should('be.visible')
+			.get('.chips')
+			.should('be.visible')
+			.get('.eggs')
+			.should('be.visible')
+			.get('.drinks')
+			.should('be.visible')
+			.get('.tomatoes')
+			.should('be.visible')
+			.get('.what')
+			.should('be.visible')
+			.get('.we')
+			.should('be.visible')
+			.get('.question')
+			.should('be.visible')
+			.get('.eatin')
+			.should('be.visible')
+	})
+
+    it('should navigate to a selection screen after clicking on the zip code input, typing 5 numbers and hitting enter', () => {
+        cy.get('input')
+    })
+
+})
