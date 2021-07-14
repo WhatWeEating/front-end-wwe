9fda0546835895fda5caa31ec3a80487803788c3
Patrick <>
2021-07-13 20:47:01 -0400

    Add fetch call for winner

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,8 @@ export const fetchRestaurantsData = async () => {
 	console.log(`triggered fetchcall`)
 	return await fetch('mock-restaurant-data.json')
 }
+
+export const fetchWinnerData = async () => {
+  console.log('triggered fetch winner')
+  return await fetch()
+}
