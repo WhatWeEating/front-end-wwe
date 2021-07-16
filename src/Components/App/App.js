8cdcceb1562b495b731dc81010955b7d89e6bbb7
Patrick <>
2021-07-16 17:15:57 -0400

    refactor '/selection' path attributes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,13 +23,23 @@ const App = () => {
 		<Router>
 			<div className='App'>
 				<Switch>
-        <Route exact path='/' 
-          render={() => (
-            <LandingPage
-            storeData={storeData} 
-            /> 
+          <Route exact path='/' 
+            render={() => (
+              <LandingPage
+              storeData={storeData} 
+              /> 
+            )}/>
+          <Route exact path='/selection' 
+            render={() => (
+              <>
+              {restaurantsData.length > 0 && 
+                <Selection
+                restaurantsData={restaurantsData}
+                storeSelections={storeSelections} 
+                /> 
+              }
+            </>
           )}/>
-					<Route exact path='/selection' component={Selection} />
           <Route exact path='/winner' component={Winner} />
           <Route exact path='/voting' component={Form} />
 				</Switch>
