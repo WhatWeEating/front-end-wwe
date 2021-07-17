1939c3094ec3bdb2797eceabe71adc2792e5b20f
Ashish Malla <ashishmalla45@gmail.com>
2021-07-17 15:10:42 -0600

    Update fetch path until BE setups CORS

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,7 +3,8 @@ const restaurantsPath =
 
 export const fetchRestaurantsData = async (zipCode, fetchId) => {
 	console.log(`triggered fetchcall`)
-	return await fetch(`${restaurantsPath}${zipCode}&${fetchId}`)
+  return await fetch('mock-restaurant-data.json')
+	// return await fetch(`${restaurantsPath}${zipCode}&${fetchId}`)
 }
 
 export const fetchWinnerData = async (eventID) => {
