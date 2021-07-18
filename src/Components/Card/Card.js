b6807fff62fba66c70e068443d7c72011548f0a3
robertjosephderouin <robertjosephderouin@gmail.com>
2021-07-18 12:01:44 -0700

    Align items in the center

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -12,7 +12,7 @@ const Card = ({id, key, type, rating, price, phone, api_id, open, name, image_ur
       <p>{rating}</p>
       <a href={phone}>{phone}</a>
       <address>{full_address}</address>
-      <button disabled={disabled && !isSelected} onClick={() => toggleChoice(id)}>{!isSelected ? "Select" : "Unselect"}</button>    
+      <button className='select' disabled={disabled && !isSelected} onClick={() => toggleChoice(id)}>{!isSelected ? "Select" : "Unselect"}</button>    
     </div>
   )
 }
