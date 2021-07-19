7c48aa556c5cf3b287cf3e3e27756894a2c787ef
robertjosephderouin <robertjosephderouin@gmail.com>
2021-07-19 09:13:02 -0700

    Add key to card

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,9 +2,9 @@ import './Card.css';
 
 import React from 'react';
 
-const Card = ({id, key, type, rating, price, phone, api_id, open, name, image_url, full_address, toggleChoice, isSelected, disabled}) => {
+const Card = ({id, type, rating, price, phone, api_id, open, name, image_url, full_address, toggleChoice, isSelected, disabled}) => {
   return (
-    <div className='card'>
+    <div key={id} className='card'>
       <img className='image' src={image_url}/>
       <h3>{name}</h3>
       <p>{type}</p>
