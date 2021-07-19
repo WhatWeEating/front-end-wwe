2219c4d929fd8a0a44e48868bf48306f9a3c1604
Ashish Malla <ashishmalla45@gmail.com>
2021-07-19 11:01:24 -0600

    Update endpoint

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,8 +3,12 @@ const restaurantsPath =
 
 export const fetchRestaurantsData = async (zipCode, fetchId) => {
 	console.log(`triggered fetchcall`)
-  return await fetch('mock-restaurant-data.json')
-	// return await fetch(`${restaurantsPath}${zipCode}&${fetchId}`)
+  // return await fetch('mock-restaurant-data.json')
+	return await fetch(`${restaurantsPath}${zipCode}`, {
+		headers: {
+			event_id: `${fetchId}`,
+		},
+	})
 }
 
 export const fetchWinnerData = async (eventID) => {
