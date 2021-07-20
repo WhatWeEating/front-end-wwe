93674ca2e696a45b1a144ff104d2211e6bc7985a
Patrick <>
2021-07-20 17:23:11 -0400

    Intercept post

--- cypress/integration/Winner_spec.js
+++ cypress/integration/Winner_spec.js
@@ -5,9 +5,16 @@ describe('Winner Page', () => {
 
   it('should navigate to winner page', () => {
     cy.get('.input-container').type('11111').type('{enter}')
-    cy.get(':nth-child(1) > .select').click()
-    cy.get(':nth-child(2) > .select').click()
-    cy.get(':nth-child(3) > .select').click()
-    cy.get('.selection > :nth-child(3)').click()
+    .get(':nth-child(1) > .select').click()
+    .get(':nth-child(2) > .select').click()
+    .get(':nth-child(3) > .select').click()
+    cy.intercept('POST', `https://back-end-wwe.herokuapp.com/graphql`, {
+      statusCode: 201,
+      body: {
+        name: 'Peter Pan',
+  }
+    })
+    .get('.selection > :nth-child(3)').click()
+    .get('.selection-go-vote-button').click()
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,3 +8,13 @@ Cypress.Commands.add('interceptRestaurantsFetch', () => {
   })
   .visit('https://mysterious-cove-94790.herokuapp.com/')
 })
+
+Cypress.Commands.add('interceptselectionsFetch', () => {
+  cy.fixture('selections.json')
+  .then(data => {
+    cy.intercept('GET', 'https://back-end-wwe.herokuapp.com/restaurants?zip=11111', {
+    statusCode: 201,
+    body: data
+  })
+  })
+})
