bd631972d8f6da3b9d1d0e7597ad21f738ea858b
Ashish Malla <ashishmalla45@gmail.com>
2021-07-21 11:32:55 -0600

    Add test to check the first card image

--- cypress/integration/Selection_spec.js
+++ cypress/integration/Selection_spec.js
@@ -19,20 +19,33 @@ describe('Selection page and navigation to it', () => {
 			.should('have.text', 'What We Eating?')
 	})
 
-  it('should be able to see 6 results', () => {
-		cy
-			.get('input')
+	it('should be able to see 6 results', () => {
+		cy.get('input')
 			.type('11111')
 			.type('{enter}')
 			.get('.card')
-			.should('have.length', '6').get('[disabled=""]')
+			.should('have.length', '6')
+			.get('[disabled=""]')
 	})
 
-  it.only('should be able to see a disabled button', () => {
+	it('should be able to see a disabled button', () => {
 		cy.get('input')
 			.type('11111')
 			.type('{enter}')
 			.get('[disabled=""]')
 			.should('have.text', 'Not Enough Selections')
+	})
+
+	it.only('should be pats restaurant on index 0', () => {
+		cy.get('input')
+			.type('11111')
+			.type('{enter}')
+			.get('.card')
+			.get(':nth-child(1) > .image')
+			.should('have.attr', 'src')
+			.should(
+				'include',
+				'https://i.pinimg.com/564x/4d/72/02/4d7202eaef303577c46ec703f9f2a2db.jpg'
+			)
 	})
 })
