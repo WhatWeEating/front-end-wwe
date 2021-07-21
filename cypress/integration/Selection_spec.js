fd093016201fd083d643bf17216236f415943d0e
Ashish Malla <ashishmalla45@gmail.com>
2021-07-21 09:03:34 -0600

    Add test for the selection page title

--- cypress/integration/Selection_spec.js
+++ cypress/integration/Selection_spec.js
@@ -4,7 +4,18 @@ describe('Selection page and navigation to it', () => {
 	})
 
 	it('should navigate to a selection screen after clicking on the zip code input, typing 5 numbers and hitting enter', () => {
-		cy.get('input').type('11111').type('{enter}')
-    .url().should('include', '/selection')
+		cy.get('input')
+			.type('11111')
+			.type('{enter}')
+			.url()
+			.should('include', '/selection')
+	})
+
+	it.only('should be able to see sleections page title', () => {
+		cy.get('input')
+			.type('11111')
+			.type('{enter}')
+			.get('.title')
+			.should('have.text', 'What We Eating?')
 	})
 })
