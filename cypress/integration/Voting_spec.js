d160f9f7e1bae0b8cfedeafca37d690537d71c50
mcraig2342 <matthewcookecraig10@gmail.com>
2021-07-20 21:37:33 -0600

    Wrote test for drag and drop

--- cypress/integration/Voting_spec.js
+++ cypress/integration/Voting_spec.js
@@ -1,20 +1,19 @@
 describe('Selection page and navigation to it', () => {
 	beforeEach(() => {
 		cy.visit('http://localhost:3000')
-	})
-
-	it('should navigate through all previous screens', () => {
-		cy.get('input').type('11111').type('{enter}')
-		  .get(':nth-child(1) > .select').click()
-		  .get(':nth-child(2) > .select').click()
-		  .get(':nth-child(3) > .select').click()
+      .get('input').type('11111').type('{enter}')
+      .get(':nth-child(1) > .select').click()
+      .get(':nth-child(2) > .select').click()
+      .get(':nth-child(3) > .select').click()
       .get('.selection > :nth-child(3)').click()
       .get('.selection-go-vote-button').click()
-
 	})
 
   it('Should allow the user to drag and drop to vote', () => {
-    cy.get()
+    .get('.form > :nth-child(2)')
+      .trigger('mousedown', { which: 1 })
+      .trigger('mousemove', { clientX: 400, clientY: 600 })
+      .trigger('mouseup', { force: true })
 
   })
 })
